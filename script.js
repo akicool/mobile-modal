e147b2baf19b7df0793745154efa8db72bc10dd2
David <liid05754@gmail.com>
2023-05-16 22:56:26 +0300

    error email message added

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 const fieldByPhone = document.querySelector('.field__by-phone');
-const fieldEmail = document.querySelector('.by-phone--email');
+const fieldByPhoneEmail = document.querySelector('.by-phone--email');
 
-const inputEmail = document.querySelector('.field__email');
-const inputTel = document.querySelector('.field__tel');
+const fieldEmail = document.querySelector('.field__email');
+const fieldTel = document.querySelector('.field__tel');
 
 const buttonTel = document.querySelector('.button--tel');
-const fieldButton = document.querySelector('.field__button');
+const fieldButton = document.querySelector('.field__button');    
+
+const fieldEmpty = document.querySelector('.field__empty');
+
 
 fieldByPhone.addEventListener('click', event => {
     fieldByPhone.style.display = 'none';
-    fieldEmail.style.display = 'flex';
+    fieldByPhoneEmail.style.display = 'flex';
 
-    inputTel.style.display = 'flex';
-    inputEmail.style.display = 'none';
+    fieldTel.style.display = 'flex';
+    fieldEmail.style.display = 'none';
 
     buttonTel.style.display = 'flex';
     fieldButton.style.display = 'none';
 });
 
-fieldEmail.addEventListener('click', event => {
+fieldByPhoneEmail.addEventListener('click', event => {
     fieldByPhone.style.display = 'flex';
-    fieldEmail.style.display = 'none';
+    fieldByPhoneEmail.style.display = 'none';
 
-    inputTel.style.display = 'none';
-    inputEmail.style.display = 'flex';
+    fieldTel.style.display = 'none';
+    fieldEmail.style.display = 'flex';
 
     buttonTel.style.display = 'none';
     fieldButton.style.display = 'flex';
 });
 
-
+fieldButton.addEventListener('click', e => {
+    if (fieldEmail.value == '') {
+        fieldEmail.style.border = '1px solid red';
+        fieldEmpty.style.display = 'block';
+    } else if (fieldEmail.value == 'a') {
+        fieldEmail.style.border = '1px solid green';
+        fieldEmpty.style.display = 'none';
+    } 
+});
