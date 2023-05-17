8b61d6c351278d6e6a4113568eb2af8e42079c45
David <liid05754@gmail.com>
2023-05-17 23:50:47 +0300

    class fixed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const fieldByPhone = document.querySelector('.btn__by-phone');
-const fieldByPhoneEmail = document.querySelector('.by-phone--email');
+const fieldByPhoneEmail = document.querySelector('.btn__by-phone--email');
 
 const fieldEmail = document.querySelector('.field__email');
 const fieldTel = document.querySelector('.field-tel');
 
 const buttonTel = document.querySelector('.button--tel');
-const fieldButton = document.querySelector('.field__button');    
+const formButton = document.querySelector('.form__button');    
 
 const fieldEmpty = document.querySelector('.field__empty-email');
 
@@ -17,7 +17,7 @@ fieldByPhone.addEventListener('click', event => {
     fieldEmail.style.display = 'none';
 
     buttonTel.style.display = 'flex';
-    fieldButton.style.display = 'none';
+    formButton.style.display = 'none';
 
     fieldEmpty.style.display = 'none';
     fieldEmail.style.border = '1px solid #d1d7e8';
@@ -31,10 +31,10 @@ fieldByPhoneEmail.addEventListener('click', event => {
     fieldEmail.style.display = 'flex';
 
     buttonTel.style.display = 'none';
-    fieldButton.style.display = 'flex';
+    formButton.style.display = 'flex';
 });
 
-fieldButton.addEventListener('click', e => {
+formButton.addEventListener('click', e => {
     if (fieldEmail.value == '') {
         fieldEmail.style.border = '1px solid red';
         fieldEmpty.style.display = 'block';
