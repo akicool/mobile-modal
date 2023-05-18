697eb39945800631d168b2c7ee2471642bda1bcc
David <liid05754@gmail.com>
2023-05-18 20:30:15 +0300

    full modal fixed

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
-const fieldByPhone = document.querySelector('.btn__by-phone');
-const fieldByPhoneEmail = document.querySelector('.btn__by-phone--email');
+const methodTel = document.getElementById('login-method-by-tel');
+const methodEmail = document.getElementById('login-method-by-email');
 
-const fieldEmail = document.querySelector('.field__email');
-const fieldTel = document.querySelector('.field-tel');
+const field = document.querySelector('.field');
+const fieldEmail = document.querySelector('.field--email');
+const fieldPhone = document.querySelector('.field--phone');
 
-const buttonTel = document.querySelector('.button--tel');
-const formButton = document.querySelector('.form__button');    
+const loginEmail = document.getElementById('login-email');
+const loginTel = document.getElementById('login-tel');
+const btnIcon = document.querySelector('.btn--icon');    
 
-const fieldEmpty = document.querySelector('.field__empty-email');
+const errorEmail = document.getElementById('error-email');
+const errorTel = document.getElementById('error-tel');
 
-fieldByPhone.addEventListener('click', event => {
-    fieldByPhone.style.display = 'none';
-    fieldByPhoneEmail.style.display = 'flex';
+const errorEmailText = document.getElementById('error-email-text');
+const errorTelText = document.getElementById('error-tel-text');
 
-    fieldTel.style.display = 'flex';
+
+methodTel.addEventListener('click', event => {
+    methodTel.style.display = 'none';
+    methodEmail.style.display = 'flex';
+
+    loginEmail.style.display = 'none';
+    loginTel.style.display = 'flex';
+
     fieldEmail.style.display = 'none';
-
-    buttonTel.style.display = 'flex';
-    formButton.style.display = 'none';
-
-    fieldEmpty.style.display = 'none';
-    fieldEmail.style.border = '1px solid #d1d7e8';
+    fieldPhone.style.display = 'flex';
 });
 
-fieldByPhoneEmail.addEventListener('click', event => {
-    fieldByPhone.style.display = 'flex';
-    fieldByPhoneEmail.style.display = 'none';
+methodEmail.addEventListener('click', event => {
+    methodEmail.style.display = 'none';
+    methodTel.style.display = 'flex';
 
-    fieldTel.style.display = 'none';
+    loginEmail.style.display = 'flex';
+    loginTel.style.display = 'none';
+
+    
     fieldEmail.style.display = 'flex';
-
-    buttonTel.style.display = 'none';
-    formButton.style.display = 'flex';
+    fieldPhone.style.display = 'none';
 });
 
-formButton.addEventListener('click', e => {
-    if (fieldEmail.value == '') {
-        fieldEmail.style.border = '1px solid red';
-        fieldEmpty.style.display = 'block';
-    } else if (fieldEmail.value == 'a') {
-        fieldEmail.style.border = '1px solid green';
-        fieldEmpty.style.display = 'none';
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+loginEmail.addEventListener('click', e => {
+    if (errorEmail.value == '' || errorEmail.value === ' ') {
+        errorEmail.style.border = '1px solid red';
+        errorEmailText.style.display = 'block';
+        field.style.paddingBottom = '3px';
+    } else if (validateEmail) {
+        errorEmail.style.border = '1px solid green';
+        errorEmailText.style.display = 'none';
     } 
 });
+
+loginTel.addEventListener('click', e => {
+    if (errorTel.value == '' || errorTel.value === ' ') {
+        errorTel.style.border = '1px solid red';
+        errorTelText.style.display = 'block';
+        field.style.paddingBottom = '3px';
+    } else if (validateEmail) {
+        errorTel.style.border = '1px solid green';
+        errorTelText.style.display = 'none';
+    } 
+});
+
+
