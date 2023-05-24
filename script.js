4e66717d1625f42f52f1c99eaec8fe386506afde
David <liid05754@gmail.com>
2023-05-24 17:59:47 +0300

    regist form added

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ const errorTel = document.getElementById('error-tel');
 const errorEmailText = document.getElementById('error-email-text');
 const errorTelText = document.getElementById('error-tel-text');
 
+const form = document.querySelector('.form');
+
 
 methodTel.addEventListener('click', event => {
     methodTel.style.display = 'none';
@@ -70,7 +72,7 @@ function onInputTel() {
         errorTel.style.border = '1px solid red';
         errorTelText.style.display = 'block';
         field.style.paddingBottom = '3px';
-}
+    }
 }
   
 loginTel.addEventListener('click', onInputTel);
@@ -78,3 +80,109 @@ loginTel.addEventListener('click', onInputTel);
 function isTelValid(value) {
     return TEL_REGEXP.test(value);
 }
+
+
+//regist field
+
+const registField = document.querySelector('.regist-field');
+const travelerBtn = document.getElementById('traveler');
+const organizerBtn = document.getElementById('organizer');
+
+const errorName = document.getElementById('name');
+const regErrorNameText = document.getElementById('reg-error-name-text');
+
+const errorSurname = document.getElementById('surname');
+const regErrorSurnameText = document.getElementById('reg-error-surname-text');
+
+const regErrorPassword = document.getElementById('reg-error-password');
+const regErrorPasswordText = document.getElementById('reg-error-password-text');
+
+const regErrorEmail = document.getElementById('reg-error-email');
+const regErrorEmailText = document.getElementById('reg-error-email-text');
+
+const regErrorTel = document.getElementById('reg-error-tel');
+const regErrorTelText = document.getElementById('reg-error-tel-text');
+
+const loginField = document.getElementById('login-field');
+
+
+
+loginEmail.addEventListener('click', e => {
+    if (isEmailValid(errorEmail.value)) {
+        form.style.display = 'none';
+        registField.style.display = 'flex';
+    }
+});
+
+organizerBtn.addEventListener('click', e => {
+    organizerBtn.style.backgroundColor = '#FFFFFF';
+    travelerBtn.style.backgroundColor = '#EEEEF2';
+});
+
+travelerBtn.addEventListener('click', e => {
+    travelerBtn.style.backgroundColor = '#FFFFFF';
+    organizerBtn.style.backgroundColor = '#EEEEF2';
+});
+
+
+loginField.addEventListener('click', e => {
+    if (errorName.value == '') {
+        errorName.style.border = '1px solid red';
+        regErrorNameText.style.display = 'block';
+        field.style.paddingBottom = '3px';
+    } else {
+        errorName.style.borderColor = 'green';
+        regErrorNameText.style.display = 'none';
+    }
+
+    if (errorSurname.value == '') {
+        errorSurname.style.border = '1px solid red';
+        regErrorSurnameText.style.display = 'block';
+        field.style.paddingBottom = '3px';
+    } else {
+        errorSurname.style.borderColor = 'green';
+        regErrorSurnameText.style.display = 'none';
+    }
+
+    if (regErrorPassword.value == '') {
+        regErrorPassword.style.border = '1px solid red';
+        regErrorPasswordText.style.display = 'block';
+        field.style.paddingBottom = '3px';
+    } else {
+        regErrorPassword.style.borderColor = 'green';
+        regErrorPasswordText.style.display = 'none';
+    }
+
+    if (isTelValid(regErrorTel.value)) {
+        regErrorTel.style.borderColor = 'green';
+        regErrorTelText.style.display = 'none';
+    } else {
+        regErrorTel.style.border = '1px solid red';
+        regErrorTelText.style.display = 'block';
+    }
+
+    if (isEmailValid(regErrorEmail.value)) {
+        regErrorEmail.style.borderColor = 'green';
+        regErrorEmailText.style.display = 'none';
+    } else {
+        regErrorEmail.style.border = '1px solid red';
+        regErrorEmailText.style.display = 'block';   
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
